44bef831fd707de2357cbf14fd9655fc8d52c2e8
VSreypov <sreypovvann002@gmail.com>
2024-01-16 00:20:04 +0900

    Your commit message

---
+++ Databases/test1.sql
@@ -0,0 +1,10 @@
+show databases;
+
+create database ictconverg;
+
+use ictconverg;
+
+create table student(name varchar(20), college varchar(20)); #create table
+insert into student values ('sreypov','RUPP'), ('Sokea','FE'); #insert information
+
+select * from student; #show results
